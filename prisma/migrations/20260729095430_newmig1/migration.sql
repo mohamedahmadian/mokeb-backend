40c51cbe8b70ae43f1b12a850a14c94b1332d8fe
mohamedahmadian <mohamed.ahmadian@gmail.com>
2026-07-30 14:03:15 +0330

    fix

--- prisma/migrations/20260729095430_newmig1/migration.sql
+++ prisma/migrations/20260729095430_newmig1/migration.sql
@@ -5,8 +5,7 @@
   - Made the column `defaultReservationDays` on table `mawkibs` required. This step will fail if there are existing NULL values in that column.
 
 */
--- DropIndex
-DROP INDEX "users_servantMawkibId_idx";
+
 
 -- AlterTable
 ALTER TABLE "mawkibs" ALTER COLUMN "maxReservationDays" SET NOT NULL,
